a9817ee4b5e0b5788cc1d00e7e64552eedba219d
andre <aansah474@gmail.com>
2024-12-03 21:19:53 +0700

    rev:minmax

--- app/Http/View/Composers/NotifComposer.php
+++ app/Http/View/Composers/NotifComposer.php
@@ -3,6 +3,7 @@
 namespace App\Http\View\Composers;
 
 use App\Models\MinMaxProduk;
+use App\Models\VKartuStok;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
@@ -20,19 +21,34 @@ class NotifComposer
 	 */
 	public function compose(View $view): void
 	{
-		$barang_habis = MinMaxProduk::select(
-				'minmax_produk.*',
-				DB::raw('coalesce(v_kartu_stok.stok,0) as stok')
-			)->
-			with('data_produk','satuan_produk')->
-			has('satuan_produk')->
-			has('data_produk')->
-			leftJoin('v_kartu_stok', function ($join) {
-				$join->
-					on('minmax_produk.kode_produk','=','v_kartu_stok.kode_produk')->
-					on('minmax_produk.satuan_id','=','v_kartu_stok.satuan_id');
-			})->
-			get();
+		$barang_habis = VKartuStok::select([
+			'v_kartu_stok.*',
+			DB::raw('coalesce(stok,0) as stok'),
+			'minmax_produk.min_stok',
+			'minmax_produk.max_stok',
+		])->
+		with('data_produk','satuan_produk')->
+		has('data_produk')->
+		has('satuan_produk')->
+		leftJoin('minmax_produk', function ($join) {
+			$join->
+				on('v_kartu_stok.kode_produk','=','minmax_produk.kode_produk')->
+				on('v_kartu_stok.satuan_id','=','minmax_produk.satuan_id');
+		})->
+		get();
+		// $barang_habis = MinMaxProduk::select(
+		// 		'minmax_produk.*',
+		// 		DB::raw('coalesce(v_kartu_stok.stok,0) as stok')
+		// 	)->
+		// 	with('data_produk','satuan_produk')->
+		// 	has('satuan_produk')->
+		// 	has('data_produk')->
+		// 	leftJoin('v_kartu_stok', function ($join) {
+		// 		$join->
+		// 			on('minmax_produk.kode_produk','=','v_kartu_stok.kode_produk')->
+		// 			on('minmax_produk.satuan_id','=','v_kartu_stok.satuan_id');
+		// 	})->
+		// 	get();
 		$barang_habis = $barang_habis->filter(function ($v,$k) {
 			return $v->stok==0;
 		});
